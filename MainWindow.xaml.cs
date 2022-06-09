afeaad61a39a705b5b3cf8244bb9d52fcbb3cbf9
piotr233 <pdrabik223@gmail.com>
2022-06-09 20:54:37 +0200

    first balls on screen using wpf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,25 @@ namespace c_sharp_fluid_sim
     /// </summary>
     public partial class MainWindow : Window
     {
+        Point currentPoint = new Point();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void MouseDownHandler(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+            Ellipse drop = new Ellipse();
+            drop.Height = 50;
+            drop.Width = 50;
+            
+            drop.Fill = Brushes.Aqua;
+            Canvas.SetLeft(drop, currentPoint.X - drop.Width/2);
+            Canvas.SetTop(drop, currentPoint.Y- drop.Height/2);
+            screen.Children.Add(drop);
+            
         }
     }
 }
