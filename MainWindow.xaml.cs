ca9cdbfc9986b07843b7b5cc2757678301775cca
piotr233 <pdrabik223@gmail.com>
2022-06-11 08:43:28 +0200

    added cursor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,24 +22,49 @@ namespace c_sharp_fluid_sim
     public partial class MainWindow : Window
     {
         Point currentPoint = new Point();
+        private Ellipse Mouse_cursor = new Ellipse();
+        private double Radius = 12.0;
+
         public MainWindow()
         {
+            Mouse_cursor.Height = 12 * 2;
+            Mouse_cursor.Width = 12 * 2;
             InitializeComponent();
+
+            Mouse_cursor.Fill = Brushes.Aqua;
+            screen.Children.Add(Mouse_cursor);
+
         }
-        
+
         private void MouseDownHandler(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (e.ButtonState == MouseButtonState.Pressed)
                 currentPoint = e.GetPosition(this);
+            
+            DrawDroplet(currentPoint, 12);
+        }
+
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            currentPoint = e.GetPosition(this);
+            Canvas.SetLeft(screen.Children[0], currentPoint.X - Radius);
+            Canvas.SetTop(screen.Children[0], currentPoint.Y - Radius);
+            
+        }
+
+        private void DrawDroplet(Point position, double radius = default)
+        {
+            if (radius == default) radius = Radius;
+
             Ellipse drop = new Ellipse();
-            drop.Height = 50;
-            drop.Width = 50;
-            
+            drop.Height = radius * 2;
+            drop.Width = radius * 2;
+
             drop.Fill = Brushes.Aqua;
-            Canvas.SetLeft(drop, currentPoint.X - drop.Width/2);
-            Canvas.SetTop(drop, currentPoint.Y- drop.Height/2);
+
+            Canvas.SetLeft(drop, position.X - radius);
+            Canvas.SetTop(drop, position.Y - radius);
             screen.Children.Add(drop);
-            
         }
     }
 }
